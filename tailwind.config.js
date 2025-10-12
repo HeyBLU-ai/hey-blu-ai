4577c0a783b4e0e2c238abccc935f01a49e6d6e5
robreganSF <robr@bigleagueballpark.com>
2025-10-11 22:19:30 -0700

    CRITICAL FIX: Complete vercel.json routes, tailwind config, remove Jekyll config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: [
     "./rulebook/**/*.{html,js}",
+    "./pitchdeck/**/*.{html,js}",
+    "./pitchdeck2/**/*.{html,js}",
+    "./pitchdeck3/**/*.{html,js}",
     "./*.html",
     "./vision/**/*.html"
   ],
